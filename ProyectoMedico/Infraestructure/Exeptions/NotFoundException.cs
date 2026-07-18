7fcff9ea79d7014145fa79ecf45d508fecc30010
josemiliriano <josemiliriano83@gmail.com>
2026-07-18 07:54:22 -0400

    Creacion de Exepciones

---
+++ ProyectoMedico/Infraestructure/Exeptions/NotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructure.Exeptions
+{
+    public class NotFoundException : BusinessException
+    {
+        public NotFoundException(string entityName, object id) : base($"{entityName} con ID '{id}' no fue encontrado.", 404)
+        {
+        }
+    }
+
+    public class AlreadyExistsException : BusinessException
+    {
+        public AlreadyExistsException(string entityName, string value) : base($"{entityName} '{value}' ya existe.", 409)
+        {
+        }
+    }
+
+    public class ValidationException : BusinessException
+    {
+        public ValidationException(string message) : base(message, 400)
+        {
+        }
+    }
+}
