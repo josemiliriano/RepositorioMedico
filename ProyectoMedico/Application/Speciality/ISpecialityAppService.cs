010bf02648aa293cdcb7b2fbdb538e694e998015
josemiliriano <josemiliriano83@gmail.com>
2026-07-25 13:31:07 -0400

    Creacion de Interfaz y Servicio Especialidad

---
+++ ProyectoMedico/Application/Speciality/ISpecialityAppService.cs
@@ -0,0 +1,19 @@
+﻿using Application.Speciality.DTO;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Speciality
+{
+    public interface ISpecialityAppService
+    {
+        public Task<CDSpecialty> AddSpeciality(SpecialityDto dto);
+        public Task<List<CDSpecialty>> GetAllSpeciality();
+        public Task<List<CDSpecialty>> GetSpecialityNoDelete();
+        public Task<CDSpecialty> UpdateSpeciality(int id, SpecialityDto dto);
+        public Task<CDSpecialty> Softdelete(int id);
+        public Task DeleteSpeciality(int id);
+        public Task<CDSpecialty> GetSpecialityById(int id);
+    }
+}
